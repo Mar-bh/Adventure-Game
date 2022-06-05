676f61b89c50475dfde6bfecace5ffd075b4872f
Mary <esquivelmariana0702@gmail.com>
2022-06-05 17:47:52 -0500

    Item

---
+++ Item.h
@@ -0,0 +1,24 @@
+#ifndef ITEM_H
+#define ITEM_H
+#include <iostream>
+
+class Item
+{
+private:
+    std::string item;
+    std::string caracteristica;
+    int puntos;
+public:
+    Item();
+    Item(int,std::string,std::string);
+    ~Item();
+    std::string getItem() const; 
+    std::string getCaracteristica() const; 
+    int getPuntos()const;
+    void setItem(std::string);
+    void setCaracteristica(std::string);
+    void setPuntos(int);
+    void descripcion();
+};
+
+#endif 
