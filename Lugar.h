42a66a4d9ee651e1685297bcd5f966b02be93fc0
Mar Bustos <marianabhdez@gmail.com>
2022-06-05 22:25:27 -0500

    cambio clase lugar

--- Lugar.h
+++ Lugar.h
@@ -8,17 +8,19 @@
 class Lugar {
     public:
         Lugar();
-        Lugar(std::string, bool);
+        Lugar(std::string);
         Lugar* getSalida(std::string);
         Item getRecompensa(int);
         void setRecompensa(int, Item);
-        void setSalida(char, Lugar);
+        void setSalida(Lugar*,Lugar*,Lugar*,Lugar*);
         std::string getDescripcion();
+        int numSalida(std::string);
 
     private:
         Lugar* salidas[4];
         std::string descripcion;
-        Item* recompensa[];
+        std::vector <Item*> recompensa;
+        int llaves;
 
 }; 
 #endif
