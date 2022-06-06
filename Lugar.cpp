28d9e4f359f8d8ae53d866615d66e85c0cf530b3
Mar Bustos <marianabhdez@gmail.com>
2022-06-05 22:43:17 -0500

    avance metodos lugar

--- Lugar.cpp
+++ Lugar.cpp
@@ -1,6 +1,6 @@
 #include "Lugar.h"
 
-Lugar::Lugar(){
+Lugar::Lugar(){ //copia de fabiola con cambio
     descripcion = "Espacio del parque ";
     for(int i=0; i<4; i++){
         salidas[i]=nullptr;
@@ -8,7 +8,7 @@ Lugar::Lugar(){
     llaves = 0;
 }
 
-Lugar::Lugar(std::string desc){
+Lugar::Lugar(std::string desc){ //copia de fabiola con cambio
     descripcion = desc;
     for(int i=0; i<4; i++){
         salidas[i]=nullptr;
@@ -16,17 +16,33 @@ Lugar::Lugar(std::string desc){
     llaves = 0;
 }
 
-Lugar* Lugar::getSalida(std::string dir){
-
+Lugar* Lugar::getSalida(char dir){ //copia de fabiola con cambio
+    int num=numSalida(dir);
+    if(num>=0){
+        return salidas[num];
+    }
+    return nullptr;
 }
 
-void Lugar::setSalida(Lugar* n, Lugar* s, Lugar* e, Lugar* o,){
+void Lugar::setSalida(Lugar* n, Lugar* s, Lugar* e, Lugar* o){ //copia de fabiola
     salidas[0]=n;
     salidas[1]=s;
     salidas[2]=e;
     salidas[3]=o;
 }
 
-int Lugar::numSalida(std::string dir){
-    
+int Lugar::numSalida(char dir){
+    switch(dir){
+        case 'n':
+            return 0;
+        case 's':
+            return 1;
+        case 'e':
+            return 2;
+        case 'o':
+            return 3;
+        default:
+            return -1;
+    }
+
 }
