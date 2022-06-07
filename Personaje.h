2814da3c94db621abe2aa4ded6a72c99d124df96
Mary <esquivelmariana0702@gmail.com>
2022-06-06 19:26:48 -0500

    Personaje cpp, faltan metodos y definir el vector y composicion

--- Personaje.h
+++ Personaje.h
@@ -10,7 +10,7 @@ private:
     std::string nombre;
     int puntaje;
     //Personaje* inventario[1];
-    std::vector <Item*> Inventario;
+    std::vector <Item*> inventario;
     Lugar descripcion;
 
 public:
@@ -22,10 +22,13 @@ public:
     Lugar getDescripcion();
     Item* getInventario();
     void setPersonaje(std::string);
-    void setDescipcion(std::string);
+    void setDescripcion(std::string);
     void setPuntos(int);
     void setInventario(std::vector <Item*>);
-    void descripcion();
+    void muestraInventario(inventario); //para mostrar el inventario necesitamos acceder al vector inventario y observar todos los items que tenemos, es un vector porque podemos agregar mas items.
+    void agregaItems(Item);// agregamos mas items al inventario
+    void consulta();// consulta para poder ver el lugar en el que se encuentra por medio de posicion, consulta(posicion)?
+    void pelea();// opciones de pelea
 };
 
 #endif 
