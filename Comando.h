0963199876d1ea48854136b1b38e158b2ae0b038
Mary <esquivelmariana0702@gmail.com>
2022-06-12 16:20:36 -0500

    Comando

---
+++ Comando.h
@@ -0,0 +1,17 @@
+#ifndef COMANDO_H
+#define COMANDO_H
+#include <iostream>
+
+class Comando {
+    public:
+        Comando();
+        Comando(std::string, std::string);
+        std::string getComando() const;
+        void setSegPalabra(std::string);
+        bool tieneSegPalabra();
+        virtual void ejecuta() = 0;
+    private: 
+        std::string comando;
+        std::string segPalabra;
+};
+#endif
