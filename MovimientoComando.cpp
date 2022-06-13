952c715a83ceb2bf94a182cdfc505116842896ee
Mar Bustos <marianabhdez@gmail.com>
2022-06-12 22:21:18 -0500

    correcion movimiento comando

--- MovimientoComando.cpp
+++ MovimientoComando.cpp
@@ -1,4 +1,5 @@
 #include "MovimientoComando.h"
+#include <string>
 
 MovimientoComando::MovimientoComando(Personaje* _jugador){
     jugador = _jugador;
@@ -10,14 +11,15 @@ void MovimientoComando::ejecuta(){
         std::cout << "Puede ser n para norte, s para sur, e para este y o para oeste" << std::endl;
     }
     else{
-        std::string direc = getSegPalabra();
-        // if (jugador-> (direc)){
-        //    Lugar* actual= jugador->getPosicion(); necesitamos metodo para saber si la persona se va a moverr
-        //     std::cout << "Te has movido hacia el " << direc << std::endl;
-        //     std::cout << "Ahora estas en: " << actual->getDescripcion() << std::endl;
-        // }
-        // else{
-        //     std::cout << "No hay salida en esa direccion o no tienes la llave para abrir, busca otra salida... o la llave..." << std::endl;
-        // }
+        std::string direc = getSegPalabra(); 
+        char direccion = direc[0];
+        if (jugador-> camina(direccion)){
+           Lugar* actual= jugador->getPosicion(); //necesitamos metodo para saber si la persona se va a moverr
+            std::cout << "Te has movido hacia el " << direc << std::endl;
+            std::cout << "Ahora estas en: " << actual->getDescripcion() << std::endl;
+        }
+        else{
+            std::cout << "No hay salida en esa direccion o no tienes la llave para abrir, busca otra salida... o la llave..." << std::endl;
+        }
     } 
 }
