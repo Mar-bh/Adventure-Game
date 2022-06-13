ba3ff4269a06e5ba258a20df900ed49f47ba4703
Mar Bustos <marianabhdez@gmail.com>
2022-06-13 14:38:51 -0500

    se crea la clase game

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 #include "Enemigo.h"
 #include "Pollos.h"
 #include "Personaje.h"
-#include "Comado.h"
+#include "Comando.h"
 
 using namespace std;
 
@@ -16,25 +16,18 @@ int main(){
     
     inventario.push_back(new Item(20,"hacha","afilada"));
 
-    /*
-    Item pilin(300,"pilin","pilin");
-    item.push_back(new Item(100,"palito","grande"));
-    item.push_back(new Item(200,"rata tiesa","rata con Thiner"));
-    item.push_back(new Item(40,"piedra","chiquita"));
-    item.push_back(new Item(300,"pilin","pilin"));*/
-
     Lugar primerLugar("Plaza Principal");
     
 
 
-    Personaje personaje1("Nico",200,primerLugar,inventario);
-    Enemigo enemigo1("Juan",100,personaje1.getPosicion(),inventario,5); 
-    cout<<typeid(enemigo1).name();
-    cout<<"---------------------------------------------------------------------"<<endl;
-    cout<<"Hola "<<personaje1.getNombre()<<" salva a los pollos"<<endl;
-    cout<<"---------------------------------------------------------------------"<<endl;
-    cout<<personaje1.getNombre()<<" tienes que pelear con el primer enemigo en "<<primerLugar.getDescripcion()<<endl;
-    cout<<"---------------------------------------------------------------------"<<endl;
+    // Personaje personaje1("Nico",200,primerLugar,inventario);
+    // Enemigo enemigo1("Juan",100,personaje1.getPosicion(),inventario,5); 
+    // cout<<typeid(enemigo1).name();
+    // cout<<"---------------------------------------------------------------------"<<endl;
+    // cout<<"Hola "<<personaje1.getNombre()<<" salva a los pollos"<<endl;
+    // cout<<"---------------------------------------------------------------------"<<endl;
+    // cout<<personaje1.getNombre()<<" tienes que pelear con el primer enemigo en "<<primerLugar.getDescripcion()<<endl;
+    // cout<<"---------------------------------------------------------------------"<<endl;
 
     int golpes;
     //string nombreEnemigo;
@@ -51,7 +44,7 @@ int main(){
         cin >> golpes;
         //nombreEnemigo = to_string(enemigo1.getNombre());
         //temporal << enemigo1.getNombre();
-        personaje1.pelea(golpes,enemigo1);
+        //personaje1.pelea(golpes,enemigo1);
     }
     while (golpes!=7);
 }
