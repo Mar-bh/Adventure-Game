813cc85d87d72f927aa0dda767eab0e868806f98
Mar Bustos <marianabhdez@gmail.com>
2022-06-13 21:06:28 -0500

    avances

--- Enemigo.h
+++ Enemigo.h
@@ -10,13 +10,13 @@ public:
     Enemigo();
     Enemigo(std::string _nombre,int _puntaje,Lugar* _posicion,std::vector <Item*> _inventario,int _nivelPeligro);
     int getnivelPeligro() const; 
-    Personaje getNombre()const;
+    std::string getNombre()const;
     Personaje getPuntaje()const;
     Personaje getPosicion();
     void setnivelPeligro(int);    
     void setNombre(Personaje);
     void setPuntaje(Personaje);
-    void setPosicion(Personaje);
+    void setPosicion(Lugar*);
     void dialogo(); 
     void quitaPuntaje();
 };
--- Juego.cpp
+++ Juego.cpp
@@ -19,27 +19,48 @@ void Juego::creaElementos(){
 
 void Juego::creaElementos(){
     jugador = new Personaje();
-    plazaPrincipal = new Lugar("Plaza principal del Parque",0);
-    zonaJuegos = new Lugar("Zona de Juegos, muchos lugares para jugar",0);
-    lago = new Lugar("Lago de agua azul",0);
-    zonaPicnic = new Lugar("Aquí vienen a un picnic",0);
-    areneros = new Lugar("Zona para jugar con la arena",0);
-    arbolCaido = new Lugar("Uy, un arbol caido. Zona final",4);
+    zonas[0] = new Lugar("Plaza principal del Parque",0); //Plaza principal
+    zonas[1] = new Lugar("Zona de Juegos, muchos lugares para jugar",0); //zonaJuegos
+    zonas[2] = new Lugar("Lago de agua azul",0); //lago
+    zonas[3]= new Lugar("Aquí vienen a un picnic",0); //zonaPicnic 
+    zonas[4]= new Lugar("Zona para jugar con la arena",0); //areneros
+    zonas[5]= new Lugar("Uy, un arbol caido. Zona final",4); //arbol caido
     //posiciones
-    jugador -> setPosicion(plazaPrincipal); 
-    pollo1 -> setPosicion (areneros);
-    pollo2 -> setPosicion(lago);
-    pollo3 -> setPosicion(zonaJuegos);
-    pollo4 -> setPosicion(zonaPicnic);
-    // rival1 -> setPosicion(areneros);
+    jugador -> setPosicion(zonas[0]); 
+    for(int i=0;i<4;i++){
+        pollitos[i] ->setPosicion(zonas[i+1]);
+    }
+    for(int i=0;i<5;i++){
+        rivales[i] ->setPosicion(zonas[i+1]);
+    }
+    // rival1 -> setPosicion(zonas[4]);
     // rival2 -> setPosicion(lago);
     //rival3 -> setPosicion(zonaJuegos)
 
      //salidas de cada lugar
-    plazaPrincipal ->setSalida(areneros,lago,zonaJuegos,zonaPicnic);
-    areneros -> setSalida(nullptr,plazaPrincipal,zonaJuegos,nullptr );
-    zonaJuegos -> setSalida(nullptr,arbolCaido,nullptr,areneros);
-    lago -> setSalida(plazaPrincipal,nullptr,zonaPicnic,nullptr);
-    zonaPicnic -> setSalida(nullptr,nullptr,nullptr,plazaPrincipal);
+    zonas[0] ->setSalida(zonas[4],zonas[2],zonas[1],zonas[3]);
+    zonas[4] -> setSalida(nullptr,zonas[0],zonas[1],nullptr );
+    zonas[1] -> setSalida(nullptr,zonas[5],nullptr,zonas[4]);
+    zonas[2] -> setSalida(zonas[0],nullptr,zonas[3],nullptr);
+    zonas[3] -> setSalida(nullptr,nullptr,nullptr,zonas[0]);
+
+}
+
+void Juego::imprimeInicio(){
+    std::cout << "Te encuentras en un parque" << std::endl;
+    std::cout << "Tienes una mision especial " << std::endl;
+    std::cout <<"Recorre el parque y recuperalos..." << std::endl;
+    std::cout << "Si necesitas ayuda teclea la palabra: ayuda" << std::endl;
+}
+
+void Juego::imprimeFin(){
+    std::cout << "Gracias por jugar" << std::endl;
+    std::cout << "Estatus final -----" << std::endl;
+    jugador ->consulta();
+    std::cout << "FIN" << std::endl;
+}
+
+void Juego::play(){
+    imprimeInicio();
 
 }
